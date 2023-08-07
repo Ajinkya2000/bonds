a629c4ce758f8963ca7de78b95457d10da330741
Ajinkya Deshpande <ajinkya.deshpande0000@gmail.com>
2023-08-08 02:32:21 +0530

    add search and select columns

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { DataTable } from "@/components/DataTable/DataTable";
-// import { dummyData } from '@/utils/dummyData';
 import bondsapi from '@/endpoints/bondsapi';
 import { BondDataType } from '@/types';
 import { columns } from "@/utils/columns";
+import { Box, Flex, Button, InputGroup, InputLeftElement, Input } from "@chakra-ui/react";
 import { useEffect, useState } from 'react';
+import { GoMultiSelect } from 'react-icons/go';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 export default function Home() {
 
@@ -14,6 +16,27 @@ export default function Home() {
   }, []);
 
   return (
-    <DataTable columns={columns} data={data} />
+    <Box>
+      <Flex justifyContent='space-between' alignItems='center'>
+        <Flex>
+          <Button size='sm'
+            leftIcon={<GoMultiSelect size={18} />}
+            borderRadius='2px'
+            bg="gray.350"
+            border="1px"
+            borderColor="gray.550"
+            me="5"
+          >
+            Select Columns
+          </Button>
+
+          <Flex justifyContent='center' alignItems='center' border='1px' borderColor="gray.550" px='2'>
+            <AiOutlineSearch size={20} />
+            <Input size='sm' border='none' height='auto' outline='none' placeholder='Search' boxShadow='none' />
+          </Flex>
+        </Flex>
+      </Flex>
+      <DataTable columns={columns} data={data} />
+    </Box>
   )
 }
--- client/src/utils/theme.ts
+++ client/src/utils/theme.ts
@@ -4,12 +4,14 @@ const theme = extendTheme({
   colors: {
     gray: {
       250: "#FAFAFA",
+      350: "#EEEEEE",
       450: "#919191",
+      550: "#D1D8DC",
       750: "#4B4B4B",
     },
     blue: {
       250: "#C0E0F8",
-      550: "#1472c4"
+      550: "#1472c4",
     }
   }
 });
