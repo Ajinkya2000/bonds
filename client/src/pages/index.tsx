c6b97acf702873bcb359202a041e0dfa49abaf1a
Ajinkya Deshpande <ajinkya.deshpande0000@gmail.com>
2023-08-07 17:13:51 +0530

    add method to fetch data

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -6,10 +6,23 @@ import styles from '@/styles/Home.module.css'
 import { DataTable } from "@/components/DataTable/DataTable";
 import {columns} from "@/utils/columns";
 import { dummyData } from '@/utils/dummyData';
+import { useEffect } from 'react';
+import bondsapi from '@/endpoints/bondsapi';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  useEffect(() => {
+    console.log("Fetching Data.....");
+    const fetchData = async () => {
+      const res = await bondsapi.get("/book");
+      console.log(res);
+    }
+
+    // fetchData();
+  })
+
   return (
     <DataTable columns={columns} data={dummyData} />
   )
