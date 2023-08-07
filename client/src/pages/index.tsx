c8d76bebb380a7cd8174f90a178bbea82e4d092f
Ajinkya Deshpande <ajinkya.deshpande0000@gmail.com>
2023-08-08 01:04:46 +0530

    create trade page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,15 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useEffect } from 'react';
+import {Box} from '@chakra-ui/react'
 import styles from '@/styles/Home.module.css'
 
 import { DataTable } from "@/components/DataTable/DataTable";
 import {columns} from "@/utils/columns";
 import { dummyData } from '@/utils/dummyData';
-import { useEffect } from 'react';
 import bondsapi from '@/endpoints/bondsapi';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
@@ -24,6 +20,8 @@ export default function Home() {
   })
 
   return (
-    <DataTable columns={columns} data={dummyData} />
+    <Box>
+      <DataTable columns={columns} data={dummyData} />
+    </Box>
   )
 }
