62aab445b3c70f70b9517d4f09726c59e3686b84
abhi-05-04 <abhishekcharpale@gmail.com>
2023-08-08 12:32:17 +0530

    small changes in Navbar

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,10 +1,14 @@
-import { Box, Divider, Flex, Text } from '@chakra-ui/react';
+import { Box, Button, Center, Divider, Flex, Spacer, Text } from '@chakra-ui/react';
 import bondX from '@/images/bondX.png';
 import Image from 'next/image';
+import { roboto } from '@/utils/fonts';
+import { AiOutlineLogout } from "react-icons/ai";
+// import react, { Component} from "react"
+
 
 const Navbar = () => {
   return (
-    <Flex py="3" px='3' mb='4' borderBottom="1px" borderColor="gray.550" alignItems='center'>
+    <Flex py="3" px='3' mb='4' borderBottom="1px" borderColor="gray.550" alignItems='center' fontFamily={roboto}>
       <Image src={bondX} alt="logo" width='80' />
       <Box mx="4" bg='red' height='30px'>
         <Divider orientation='vertical' opacity="1" />
@@ -14,6 +18,10 @@ const Navbar = () => {
         <Text fontSize='sm'>Trades</Text>
         <Text fontSize='sm'>Books</Text>
       </Flex>
+      <Spacer/>
+      <AiOutlineLogout size={28}/>
+      <Text padding={'2'} fontSize='sm' textAlign={'center'}>chui chui  </Text>
+      {/* <Button marginLeft={'5'}>Logout</Button> */}
     </Flex>
   )
 }
